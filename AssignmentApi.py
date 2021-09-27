f72c9600ea17a3b58ecfa63d000cdbad7383a483
Anisha <anisha.basil@stonybrook.edu>
2021-09-27 13:05:19 -0400

    AssignmentAPI

---
+++ AssignmentApi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 12:36:51 2021
+
+@author: anishabasil
+"""
+import pandas as pd
+import requests
+## Get Request for 
+jsonResponse = requests.get('https://itunes.apple.com/search?term=jack+johnson')
+
+##Check for 200 status message 
+jsonResponse.status_code
+jsonResponse.json()
+Dataframe = pd.DataFrame(jsonResponse.json())
+
+
+## get request for
+jsonResponse2 = requests.get('https://itunes.apple.com/search?term=jack+johnson')
+jsonResponse2 =jsonResponse2.json()
+
+## Lists the results column
+jsonResponseResults = jsonResponse2['results']
